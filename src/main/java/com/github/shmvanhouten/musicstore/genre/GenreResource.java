c86e8d0041fc600f22db47d5c45d409bd094576c
Marco Beelen <marco@mcbeelen.net>
2017-06-22 21:18:25 +0200

    Exposed single genre

--- src/main/java/com/github/shmvanhouten/musicstore/genre/GenreResource.java
+++ src/main/java/com/github/shmvanhouten/musicstore/genre/GenreResource.java
@@ -1,11 +1,13 @@
 package com.github.shmvanhouten.musicstore.genre;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static com.github.shmvanhouten.musicstore.genre.Genre.GenreBuilder.aGenre;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
@@ -23,6 +25,11 @@ public class GenreResource {
     public List<Genre> getAll() {
         return genreRepository.getAll();
 
+    }
+
+    @RequestMapping(method = GET, path = "/{id}")
+    public Genre getById(@PathVariable("id") Long id) {
+        return aGenre().withId(id).withName("Hardcoded").build();
     }
 
 }
