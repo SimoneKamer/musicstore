af3ca9be2deacc288d8b716ec4b649fb58910f8c
Marco Beelen <marco@mcbeelen.net>
2017-06-22 21:29:42 +0200

    Switch to using Mybatis SQL builder

--- src/main/java/com/github/shmvanhouten/musicstore/genre/GenreRepositoryJdbcTempateImpl.java
+++ src/main/java/com/github/shmvanhouten/musicstore/genre/GenreRepositoryJdbcTempateImpl.java
@@ -1,13 +1,16 @@
 package com.github.shmvanhouten.musicstore.genre;
 
+import org.apache.ibatis.jdbc.SQL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import javax.print.attribute.standard.MediaSize;
 import java.util.List;
 
 import static com.github.shmvanhouten.musicstore.DatabaseStructure.GenreColumns.ID;
+import static com.github.shmvanhouten.musicstore.DatabaseStructure.GenreColumns.NAME;
 import static com.github.shmvanhouten.musicstore.DatabaseStructure.Table.GENRE;
 
 @Repository
@@ -24,13 +27,24 @@ public class GenreRepositoryJdbcTempateImpl implements GenreRepository {
 
     @Override
     public List<Genre> getAll() {
-        String selectQuery = "SELECT * FROM " + GENRE;
+        String selectQuery = new SQL()
+                .SELECT(ID)
+                .SELECT(NAME)
+                .FROM(GENRE)
+                .toString();
         return jdbcTemplate.query(selectQuery, new GenreRowMapper());
     }
 
     @Override
     public Genre getById(Long id) {
-        String selectQuery = "SELECT * FROM " + GENRE + " WHERE " + ID + " = ?";
+        String selectQuery = new SQL()
+                .SELECT(ID)
+                .SELECT(NAME)
+                .FROM(GENRE)
+                .WHERE(ID + " = ?")
+                .toString();
+
+
         Object[] params = {id};
         return jdbcTemplate.queryForObject(selectQuery, params, new GenreRowMapper());
     }
