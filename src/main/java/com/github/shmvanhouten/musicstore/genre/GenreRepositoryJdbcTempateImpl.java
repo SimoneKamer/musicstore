d0fad5649ce188409a3f472a552a0b8f56b92192
Marco Beelen <marco@mcbeelen.net>
2017-06-22 21:00:00 +0200

    Switch to injecting a JdbcTemplate

--- src/main/java/com/github/shmvanhouten/musicstore/genre/GenreRepositoryJdbcTempateImpl.java
+++ src/main/java/com/github/shmvanhouten/musicstore/genre/GenreRepositoryJdbcTempateImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,13 +13,13 @@ public class GenreRepositoryJdbcTempateImpl implements GenreRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
+
+
     @Autowired
-    public GenreRepositoryJdbcTempateImpl(DataSource dataSource) {
-        System.out.println("Got a DataSource");
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    public GenreRepositoryJdbcTempateImpl(JdbcTemplate jdbcTemplate) {
+        System.out.println("Got a JdbcTemplate");
+        this.jdbcTemplate = jdbcTemplate;
     }
-
-
 
 
     @Override
