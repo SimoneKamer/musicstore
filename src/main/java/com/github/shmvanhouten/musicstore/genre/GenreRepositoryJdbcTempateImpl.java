91a205c928580f0c5b3f31cd0f05c721e94b755b
Marco Beelen <marco@mcbeelen.net>
2017-06-22 21:37:37 +0200

    Switch to NamedParameterJdbcTemplate

--- src/main/java/com/github/shmvanhouten/musicstore/genre/GenreRepositoryJdbcTempateImpl.java
+++ src/main/java/com/github/shmvanhouten/musicstore/genre/GenreRepositoryJdbcTempateImpl.java
@@ -3,25 +3,29 @@ package com.github.shmvanhouten.musicstore.genre;
 import org.apache.ibatis.jdbc.SQL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-import javax.print.attribute.standard.MediaSize;
 import java.util.List;
 
 import static com.github.shmvanhouten.musicstore.DatabaseStructure.GenreColumns.ID;
 import static com.github.shmvanhouten.musicstore.DatabaseStructure.GenreColumns.NAME;
 import static com.github.shmvanhouten.musicstore.DatabaseStructure.Table.GENRE;
+import static com.github.shmvanhouten.musicstore.util.NamedParamUtil.namedParam;
 
 @Repository
 public class GenreRepositoryJdbcTempateImpl implements GenreRepository {
 
 
     private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Autowired
     public GenreRepositoryJdbcTempateImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
     }
 
 
@@ -41,13 +45,16 @@ public class GenreRepositoryJdbcTempateImpl implements GenreRepository {
                 .SELECT(ID)
                 .SELECT(NAME)
                 .FROM(GENRE)
-                .WHERE(ID + " = ?")
+                .WHERE(ID + " = " + namedParam(ID))
                 .toString();
 
 
-        Object[] params = {id};
-        return jdbcTemplate.queryForObject(selectQuery, params, new GenreRowMapper());
+        SqlParameterSource params = new MapSqlParameterSource(ID, id);
+
+        return namedParameterJdbcTemplate.queryForObject(selectQuery, params, new GenreRowMapper());
     }
+
+
 
 
 }
