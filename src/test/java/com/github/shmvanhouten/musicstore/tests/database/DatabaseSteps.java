278d9c6c4fd869fc51f92fc52f66d001e3584319
Marco Beelen <marco@mcbeelen.net>
2017-07-06 20:33:02 +0200

    Added configuration to run Cucumber features with steps

---
+++ src/test/java/com/github/shmvanhouten/musicstore/tests/database/DatabaseSteps.java
@@ -0,0 +1,12 @@
+package com.github.shmvanhouten.musicstore.tests.database;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+
+public class DatabaseSteps {
+    @Given("^the chinook database is loaded$")
+    public void theChinookDatabaseIsLoaded() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
